8ec84a97ba7805b3a9b8a76081a3e95e75b5164f
Ivan Ustyantsev <vangold915@gmail.com>
2024-07-12 13:01:51 +0800

    Portfolio: create ThemeService

--- src/services/ThemeContext.ts
+++ src/services/ThemeContext.ts
@@ -1,8 +1,8 @@
-import React from 'react'
+import React, { createContext } from 'react'
 
 export const themes = {
   dark: 'dark',
   light: 'light',
 }
 
-export const ThemeContext = React.createContext({})
+export const ThemeContext = createContext({})
