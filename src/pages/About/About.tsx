83d894b3f42d31744e6d3da5dc1ee8b214e07891
Ivan Ustyantsev <vangold915@gmail.com>
2024-07-22 21:56:09 +0800

    porfolio desctop version is completed

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import styles from './about.module.scss'
 import { AboutRightSidebar } from 'src/components'
 import { Link } from 'react-router-dom'
@@ -6,27 +6,52 @@ import { useTranslation } from 'react-i18next'
 
 export const About = () => {
   const { t } = useTranslation()
+  const printedText = () => {
+    const nodes = document.querySelectorAll('#animate')
+    nodes
+    .forEach((item, index, array) => {
+      if (index !== array.length - 1) {
+        setTimeout(() => {
+          item.classList.add(`${styles.about__typing}`)
+          item.classList.remove(`${styles.about__text_hidden}`)
+        }, index * 1500)
+      }
+      else {
+        setTimeout(() => {
+          item.classList.add(`${styles.about__typingEnd}`)
+          item.classList.remove(`${styles.about__text_hidden}`)
+        }, index * 1500)
+      }
+    })
+    
+  }
+
+  useEffect(() => {
+    printedText()
+  }, [])
+
   return (
     <div className={styles.about}>
       <div className={styles.about__main}>
         <div className={styles.about__content}>
           <h1>{t('screens.about.title')}</h1>
-          <div className={styles.about__text}>
-            <p>function showSkills() {'{'}</p>
-            <p>&nbsp;&nbsp;const skills = {'['}</p>
-            <p>&nbsp;&nbsp;&nbsp;&nbsp;`HTML`, `CSS`, `JavaScript`, `React`, `Git`, </p>
-            <p>&nbsp;&nbsp;&nbsp;&nbsp;`ООП`, `C/C++`, `React`, `Gulp`, `Redux`,</p>
-            <p>&nbsp;&nbsp;&nbsp;&nbsp;`ReactJS`, `Webpack`, `SOLID`, `ES6`, </p>
-            <p>&nbsp;&nbsp;&nbsp;&nbsp;`Redux-Saga`, `Redux-thunk`, `C#`,</p>
-            <p>&nbsp;&nbsp;&nbsp;&nbsp;`TypeScript`,  `MySQL`, `Linux`, `Python`,</p>
-            <p>&nbsp;&nbsp;&nbsp;&nbsp;`Java`, `Docker`, `Sass`, `Less`, `Bootstrap`, </p>
-            <p>&nbsp;&nbsp;&nbsp;&nbsp;`Jest`, `Firebase`</p>
-            <p>&nbsp;&nbsp;{'];'}</p>
-            <p>&nbsp;&nbsp;{'console.log(\'My skills:\');'}</p>
-            <p>&nbsp;&nbsp;{'skills.forEach(skill => {'}</p>
-            <p>&nbsp;&nbsp;&nbsp;&nbsp;console.log(skill);</p>
-            <p>&nbsp;&nbsp;{'})'};</p>
-            <p>{'}'};</p>
+          <div id='code' className={styles.about__text}>
+            <p id='animate' className={styles.about__text_hidden}>function showSkills() {'{'}</p>
+            <p id='animate' className={styles.about__text_hidden}>&nbsp;&nbsp;const skills = {'['}</p>
+            <p id='animate' className={styles.about__text_hidden}>&nbsp;&nbsp;&nbsp;&nbsp;`HTML`, `CSS`, `JavaScript`, `React`, `Git`, </p>
+            <p id='animate' className={styles.about__text_hidden}>&nbsp;&nbsp;&nbsp;&nbsp;`ООП`, `C/C++`, `React`, `Gulp`, `Redux`,</p>
+            <p id='animate' className={styles.about__text_hidden}>&nbsp;&nbsp;&nbsp;&nbsp;`ReactJS`, `Webpack`, `SOLID`, `ES6`, </p>
+            <p id='animate' className={styles.about__text_hidden}>&nbsp;&nbsp;&nbsp;&nbsp;`Redux-Saga`, `Redux-thunk`, `C#`,</p>
+            <p id='animate' className={styles.about__text_hidden}>&nbsp;&nbsp;&nbsp;&nbsp;`TypeScript`,  `MySQL`, `Linux`, `Python`,</p>
+            <p id='animate' className={styles.about__text_hidden}>&nbsp;&nbsp;&nbsp;&nbsp;`Java`, `Docker`, `Sass`, `Less`, `Bootstrap`, </p>
+            <p id='animate' className={styles.about__text_hidden}>&nbsp;&nbsp;&nbsp;&nbsp;`Jest`, `Firebase`</p>
+            <p id='animate' className={styles.about__text_hidden}>&nbsp;&nbsp;{'];'}</p>
+            <p id='animate' className={styles.about__text_hidden}>&nbsp;&nbsp;{'console.log(\'My skills:\');'}</p>
+            <p id='animate' className={styles.about__text_hidden}>&nbsp;&nbsp;{'skills.forEach(skill => {'}</p>
+            <p id='animate' className={styles.about__text_hidden}>&nbsp;&nbsp;&nbsp;&nbsp;console.log(skill);</p>
+            <p id='animate' className={styles.about__text_hidden}>&nbsp;&nbsp;{'})'};</p>
+            <p id='animate' className={styles.about__text_hidden}>{'}'};</p>
+            
           </div>
         </div>
         <Link className={`${styles.about__btn} primary-btn text`} to={'projects'}>[{t('screens.about.button')}]</Link>
--- src/pages/Contacts/Contacts.tsx
+++ src/pages/Contacts/Contacts.tsx
@@ -2,37 +2,79 @@ import { ContactsRightSidebar } from 'src/components/ContactsRightSidebar/Contac
 import styles from './contacts.module.scss';
 import { useTranslation } from 'react-i18next';
 import { ContactsButtons } from 'src/components';
+// import { useEffect, useState } from 'react';
+import i18next from 'i18next';
 
 
 export const Contacts = () => {
   const { t } = useTranslation()
-  let startTitle = ''
-  let middleTitle = ''
-  let endTitle = ''
-  t('screens.contacts.title')
-    .split(' ')
-    .map((item, index, array) => {
-      if (index !== array.length - 1) {
-        startTitle += item.toString() + ' '
-      }
-      if (index === array.length - 1) {
-        const temp = item.split('')
-        const lastSign = temp[temp.length - 1]
-        if (lastSign === '?') {
-          endTitle += lastSign
-        } else {
-          endTitle = ''
-        }
-        middleTitle += temp.join('')
-      }
-    })
-    
+  const titleHTML = ''
+
+  const getTitle = () => {
+    if (i18next.language === 'en') {
+      let firstWord = ''
+      let secondWord = ''
+      let lastWord = ''
+      let lastSign = ''
+      t('screens.contacts.title')
+        .split(' ')
+        .map((item, index) => {
+          if (index === 0) {
+            firstWord += item + ' '
+          }
+          if (index === 1) {
+            secondWord += item
+          }
+          if (index === 2) {
+            const tempArr = item.split('')
+            lastSign = tempArr.pop() || ''
+            lastWord = tempArr.join('')
+          }
+        })
+        return (
+          <h2 className={styles.contacts__title}>{firstWord} {secondWord}&nbsp;<span className={styles.contacts__titleSpan}>{lastWord}</span>{lastSign}</h2>
+        )
+    }
+    if (i18next.language === 'ru') {
+      let firstWord = ''
+      let secondWord = ''
+      let thirdWord = ''
+      let fourthWord = ''
+      let lastWord = ''
+      t('screens.contacts.title')
+        .split(' ')
+        .map((item, index) => {
+          if (index === 0) {
+            firstWord += item + ' '
+          }
+          if (index === 1) {
+            secondWord += item
+          }
+          if (index === 2) {
+            thirdWord += item + ' '
+          }
+          if (index === 3) {
+            fourthWord += item
+          }
+          if (index === 4) {
+            lastWord += item
+          }
+        })
+        return (
+          <h2 className={styles.contacts__title}>{firstWord} {secondWord}&nbsp;{thirdWord}<span className={styles.contacts__titleSpan}>{fourthWord}&nbsp;{lastWord}</span></h2>
+        )
+    }
+    return <h2>Test</h2>
+  }
+
+
+
   return (
     <div className={styles.contacts}>
       <div className={styles.contacts__main}>
         <div className={styles.contacts__content}>
-          <h2 className={styles.contacts__title}>{startTitle}<span className={styles.contacts__titleSpan}>{middleTitle}</span>{endTitle}</h2>
-          <ContactsButtons parentKey='contactsMain'/>
+          {getTitle()}
+          <ContactsButtons parentKey='contactsMain' />
         </div>
       </div>
       <div className={styles.contacts__rightSidebar}>
