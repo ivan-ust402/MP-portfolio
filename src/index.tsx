039d21fc663bd950601d14dc88d0b7ac82b9f620
Ivan Ustyantsev <vangold915@gmail.com>
2024-10-01 01:26:14 +0800

    Store connected to the application

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,9 @@ import 'src/assets/fonts/stylesheet.css'
 import 'src/index.scss'
 import { App } from 'src/App'
 import { Provider } from 'react-redux'
-import { store } from './store'
+import { setupStore } from './store'
+
+const store = setupStore()
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
