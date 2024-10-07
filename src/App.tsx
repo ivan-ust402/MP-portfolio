67a9d3a935f7146afa8b1f0bb506d8a0eaa9ccf9
Ivan Ustyantsev <vangold915@gmail.com>
2024-10-07 15:35:07 +0700

    test 2

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,20 @@ export const App = () => {
   const { changeBurgerStatus } = burgerSlice.actions
   const { setViewportHeight } = viewportHeightSlice.actions
 
+  if (height === 0) {
+    dispatch(setViewportHeight(
+      window.visualViewport 
+      ? window.visualViewport.height
+      : window.innerHeight
+    ))
+  }
+  if (desktop === null) {
+    dispatch(setDesktopResolution(window.innerWidth > firstBreakpoint))
+  }
+
+  if (desktop === null || desktop) {
+    dispatch(changeBurgerStatus(false))
+  }
   const desktopRef = useRef<null|boolean>(null)
 
   const handleSetHeight = useCallback(() => {
@@ -44,30 +58,16 @@ export const App = () => {
     }
   }, [dispatch, firstBreakpoint, setDesktopResolution])
 
+  
   useEffect(() => {
-    dispatch(setViewportHeight(
-      window.visualViewport 
-      ? window.visualViewport.height
-      : window.innerHeight
-    ))
     document.documentElement.style.setProperty('--vpheight', `${height}px`)
-    if (desktop === null) {
-      dispatch(setDesktopResolution(window.innerWidth > firstBreakpoint))
-    }
-  
-    if (desktop === null || desktop) {
-      dispatch(changeBurgerStatus(false))
-    }
-  }, [])
-  
-  useEffect(() => {
     window.addEventListener('resize', handleResize)
     window.visualViewport?.addEventListener('resize', handleSetHeight)
     return () => {
       window.removeEventListener('resize', handleResize)
       window.visualViewport?.removeEventListener('resize', handleSetHeight);
     }
-  }, [handleResize, handleSetHeight])
+  }, [handleResize, handleSetHeight, height])
  
   return (
     <I18nProvider>
